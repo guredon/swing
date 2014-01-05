f24849a0dab89f6d2cc4ec7228ceac568eb85367
e115715 <e115715@ie.u-ryukyu.ac.jp>
2014-01-05 18:55:55 +0900

    swing 01

---
+++ src/jp/kanpe/eclipseswing/SampleApp.java
@@ -0,0 +1,15 @@
+package jp.kanpe.eclipseswing;
+import javax.swing.JFrame;
+
+public class SampleApp {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+JFrame f = new JFrame();
+f.setSize(300,200);
+f.setVisible(true);
+}
+
+}
